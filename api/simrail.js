b8b3e2b6f3ba4a91a1d0366dba7834bbd8a0171b
knowicki942-hash <knowicki942@gmail.com>
2026-01-21 20:28:53 +0100

    Update simrail.js

--- api/simrail.js
+++ api/simrail.js
@@ -1,15 +1,22 @@
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-
   const server = req.query.server || "pl3";
 
   try {
+    // Pobieramy rozkład
     const response = await fetch(
       `https://api1.aws.simrail.eu:8082/api/getEDRTimetables?serverCode=${server}`
     );
+    const timetable = await response.json();
 
-    const data = await response.json();
-    res.status(200).json(data);
+    // Pobieramy strefę czasową (serwer-serwer nie ma błędu CORS)
+    const tzResponse = await fetch(
+      `https://api1.aws.simrail.eu:8082/api/getTimeZone?serverCode=${server}`
+    );
+    const timeZone = await tzResponse.json();
+
+    // Wysyłamy oba obiekty naraz
+    res.status(200).json({ timetable, timeZone });
 
   } catch (err) {
     res.status(500).json({ error: true });
